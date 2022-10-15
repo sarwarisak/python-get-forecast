c2a0efdb27b96d62b70094fdc68dc596840d8a82
sarwarisak <34421305+sarwarisak@users.noreply.github.com>
2022-10-15 18:14:24 -0400

    Update script.py

--- script.py
+++ script.py
@@ -4,22 +4,51 @@ import requests
 import pandas as pd
 
 
-def get_forecast( city='Pittsburgh' ):
-    '''
-    Returns the nightly's forecast for a given city.
-    Inputs:
-    city (string): A valid string
-    Output:
-    period (dictionary/JSON): a dictionary containing at least, the forecast keys startTime, endTime and detailedForecast.
-    Throws:
-    CityNotFoundError if geopy returns empty list or if the latitude longitude fields are empty.
-    ForecastUnavailable if the period is empty or the API throws any status code that is not 200
-    Hint:
-    * Return the period that is labeled as "Tonight"
-    '''
+class CityNotFoundError(Exception):
+    def _init_(self, msg):
+        super()._init_(msg)
 
-    raise NotImplementedError()
+
+class ForecastUnavailable(Exception):
+    def _init_(self, msg):
+        super()._init_(msg)
+
+
+def get_forecast(city='Pittsburgh'):
+    geolocator = Nominatim(user_agent="ssarwari")
+    location = geolocator.geocode(city)
+    if location==None:
+        raise CityNotFoundError("Latitude and Longitude unavailable.")
+        return
     
+    lats = location.latitude
+    longs = location.longitude
+
+    if (lats is None) or (longs is None):
+        raise CityNotFoundError("Latitude and Longitude unavailable.")
+        return
+
+    url = f'https://api.weather.gov/points/{lats},{longs}'
+    response = requests.get(ur;)
+    if (response.status_code != 200):
+        raise ForecastUnavailable("Status code unsuccessful.")
+        return
+
+    url2 = response.json()['properties']['forecast']
+    response2 = requests.get(url2)
+    info = response2.json()['properties']['periods']
+
+    for i in range(len(info)):
+        if (info[i]["name"] == "Tonight"):
+            start_time = info[i]['startTime']
+            end_time = info[i]['endTime']
+            detailed_forecast = info[i]['detailedForecast']
+
+    period = {"startTime": start_time,
+              "endTime": end_time,
+              "detailedForecast": detailed_forecast}
+    return period
+
 
 def main():
     period = get_forecast()
@@ -27,23 +56,30 @@ def main():
     file = 'weather.pkl'
 
     if Path(file).exists():
-        df = pd.read_pickle( file )
+        df = pd.read_pickle(file)
     else:
         df = pd.DataFrame(columns=['Start Date', 'End Date', 'Forecast'])
 
-    df = df.append({'Start Date': period['startTime'], 'End Date': period['endTime'], 'Forecast': period['detailedForecast']}, ignore_index=True)
+    df = df.append({'Start Date': period['startTime'],
+                    'End Date': period['endTime'],
+                    'Forecast': period['detailedForecast']},
+                   ignore_index=True)
     df = df.drop_duplicates()
     df.to_pickle(file)
 
-    #sort repositories
+    '''sort repositories'''
     file = open("README.md", "w")
-    file.write('![Status](https://github.com/icaoberg/python-get-forecast/actions/workflows/build.yml/badge.svg)\n')
-    file.write('![Status](https://github.com/icaoberg/python-get-forecast/actions/workflows/pretty.yml/badge.svg)\n')
+    file.write('![Status](https://github.com/sarwarisak/python-' +
+               'get-forecast/actions/workflows/build.yml/badge.svg)\n')
+    file.write('![Status](https://github.com/sarwarisak/python-' +
+               'get-forecast/actions/workflows/pretty.yml/badge.svg)\n')
     file.write('# Pittsburgh Nightly Forecast\n\n')
-    
+
     file.write(df.to_markdown(tablefmt='github'))
-    file.write('\n\n---\nCopyright © 2022 Pittsburgh Supercomputing Center. All Rights Reserved.')
+    file.write('\n\n---\nCopyright © 2022 Pittsburgh ' +
+               'Supercomputing Center. All Rights Reserved.')
     file.close()
+
 
 if __name__ == "__main__":
     main()
